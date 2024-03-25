7bf8aa55a1047a4773d72f949955ebd39502501f
Nahuel Jose Roldan <>
2024-03-24 23:32:22 -0300

    Add stack with texts, flags, and a color background.

--- swiftui-guesstheflag2/ContentView.swift
+++ swiftui-guesstheflag2/ContentView.swift
@@ -8,15 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of:")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundStyle(.white)
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        // tap action
+                    }, label: {
+                        Image(countries[number])
+                    })
+                }
+            }
         }
-        .padding()
     }
+    
+    
 }
 
 #Preview {
