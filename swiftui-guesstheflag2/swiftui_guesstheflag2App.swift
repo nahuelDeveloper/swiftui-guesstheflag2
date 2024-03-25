17931ca8c4a8b359078e9ebeb6e39f95591f1af6
Nahuel Jose Roldan <>
2024-03-24 23:20:36 -0300

    Initial commit.

---
+++ swiftui-guesstheflag2/swiftui_guesstheflag2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_guesstheflag2App.swift
+//  swiftui-guesstheflag2
+//
+//  Created by Nahuel Jose Roldan on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_guesstheflag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
